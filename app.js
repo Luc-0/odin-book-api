adce7502ce45b11dd098c1edcc8ad61aeac37384
Luc-0 <luc0x00@gmail.com>
2021-08-11 10:05:15 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGODB;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
