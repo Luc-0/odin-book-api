43cea45e6b226e7e64da063c3c36d306208cae72
Luc-0 <luc0x00@gmail.com>
2021-08-16 20:08:14 -0300

    Change friend model

--- models/user.js
+++ models/user.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true, minLength: 3, maxLength: 50},
-  username: { type: String, required: true, minLength: 3, maxLength: 50},
-  password: { type: String, required: true, minLength: 6},
-  createdAt: { type: Date, default: Date.now, immutable: true}
+  name: { type: String, required: true, minLength: 3, maxLength: 50 },
+  username: { type: String, required: true, minLength: 3, maxLength: 50 },
+  password: { type: String, required: true, minLength: 6 },
+  createdAt: { type: Date, default: Date.now, immutable: true },
+  friend: {
+    list: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+    requests: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
